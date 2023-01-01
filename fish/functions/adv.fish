0f54fa566b9c05f3de3b78ead5c94430471f2dff
Ammar Abou Zor <ammarabouzor@gmail.com>
2023-01-01 09:45:55 +0100

    before xfce

--- fish/functions/adv.fish
+++ fish/functions/adv.fish
@@ -1,3 +1,3 @@
 function adv --wraps='cd ~/Desktop/advent_of_code/ ; hx .' --description 'alias adv cd ~/Desktop/advent_of_code/ ; hx .'
-  cd ~/Desktop/advent_of_code/ ; hx . $argv; 
+  cd ~/repos/advent_of_code/; 
 end
