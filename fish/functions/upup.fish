66721238240314dc9efb0ee6e2d55b5bd44060ae
Ammar Abou Zor <ammarabouzor@gmail.com>
2023-01-03 12:48:30 +0100

    flatpak

--- fish/functions/upup.fish
+++ fish/functions/upup.fish
@@ -1,3 +1,6 @@
 function upup --wraps='sudo dnf update' --description 'alias upup sudo dnf update'
+  echo ========== dnf ==========;
   sudo dnf update $argv; 
+  echo ======== flatpak ========;
+  flatpak update;
 end
