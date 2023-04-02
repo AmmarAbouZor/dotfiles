6295204e3431cb2e01f16eb9e1d3a57ee42f3add
Ammar Abou Zor <ammarabouzor@gmail.com>
2023-04-02 10:44:24 +0200

    telescope prompt top

---
+++ nvim/lua/plugins/telescope.lua
@@ -0,0 +1,13 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  opts = {
+    defaults = {
+      sorting_strategy = "ascending",
+      layout_config = {
+        horizontal = {
+          prompt_position = "top",
+        },
+      },
+    },
+  },
+}
