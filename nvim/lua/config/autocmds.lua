9789bd7d4df3e31f587fd7a3445c8387bfca9770
Ammar Abou Zor <ammarabouzor@gmail.com>
2023-04-30 00:19:08 +0200

    Neovim add fugitive

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -7,3 +7,15 @@
 --  pattern = { "*" },
 --  command = "normal zx zR",
 -- )
+
+-- close some fugitive with <q>
+vim.api.nvim_create_autocmd("FileType", {
+  group = vim.api.nvim_create_augroup("Close_fugitive_q", { clear = true }),
+  pattern = {
+    "fugitive",
+  },
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+    vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
+  end,
+})
