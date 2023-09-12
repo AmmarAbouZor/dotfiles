f14b2cd8fd0fdf3c9ddc794ac22d331b5007272c
Ammar Abou Zor <ammarabouzor@gmail.com>
2023-09-12 21:57:11 +0200

    Neovim: Noice keybinding & tmux bindings
    Kitty & Alacritty fonts and stuff

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -29,6 +29,14 @@ return {
   --   },
   -- },
 
+  -- stylua: ignore
+keys = {
+    { "<leader>nl", function() require("noice").cmd("last") end, desc = "Noice Last Message" },
+    { "<leader>nh", function() require("noice").cmd("history") end, desc = "Noice History" },
+    { "<leader>na", function() require("noice").cmd("all") end, desc = "Noice All" },
+    { "<leader>nd", function() require("noice").cmd("dismiss") end, desc = "Dismiss All" },
+  },
+
   opts = {
     presets = {
       lsp_doc_border = true, -- add a border to hover docs and signature help
--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -1,8 +1,17 @@
 return {
   "folke/which-key.nvim",
-  opts = {
-    window = {
-      border = "single", -- none, single, double, shadow
-    },
-  },
+  opts = function(_, opts)
+    if require("lazyvim.util").has("noice.nvim") then
+      opts.defaults["<leader>sn"] = { name = "+noice" }
+      opts.defaults["<leader>n"] = { name = "+noice" }
+    end
+    opts.window = {
+      border = "single",
+    }
+  end,
+  -- opts = {
+  --   window = {
+  --     border = "single", -- none, single, double, shadow
+  --   },
+  -- },
 }
