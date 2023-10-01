bb2de602672eff8477c65051637c9a3ee7c83ee3
Ammar Abou Zor <ammarabouzor@gmail.com>
2023-10-01 22:53:32 +0200

    Neovim: TODO Comments colors & restore font size

---
+++ nvim/lua/plugins/todo_comments.lua
@@ -0,0 +1,16 @@
+return {
+  "folke/todo-comments.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  -- highlighting of the line containing the todo comment
+  -- * before: highlights before the keyword (typically comment characters)
+  -- * keyword: highlights of the keyword
+  -- * after: highlights after the keyword (todo text)
+  opts = {
+    highlight = {
+      multiline = false, -- enable multine todo comments
+      before = "", -- "fg" or "bg" or empty
+      keyword = "wide", -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty. (wide and wide_bg is the same as bg, but will also highlight surrounding characters, wide_fg acts accordingly but with fg)
+      after = "", -- "fg" or "bg" or empty
+    },
+  },
+}
