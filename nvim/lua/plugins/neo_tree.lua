3e58a9fa6aa49e29ebec5f1d8ba9573cb380f30d
Ammar Abou Zor <ammarabouzor@gmail.com>
2024-01-18 20:47:11 +0100

    Neovim: Disable lazyvim config on neo-tree
    - The configs could be a memory leak reason

--- nvim/lua/plugins/neo_tree.lua
+++ nvim/lua/plugins/neo_tree.lua
@@ -1,5 +1,6 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
+
   opts = {
     buffers = {
       window = {
@@ -9,4 +10,9 @@ return {
       },
     },
   },
+
+  -- Disable default lazygit settings to hunt down the memory leak when moving and deleting files
+  config = function(_, opts)
+    require("neo-tree").setup(opts)
+  end,
 }
