0cc4348778d257e03b9e21bdb30df968b8ff5df8
Ammar Abou Zor <ammarabouzor@gmail.com>
2024-01-20 21:00:48 +0100

    Neovim: Improve opening with directory path arguments
    - Extend neo-tree to handle this without flipping when the app starts
    - Add then comment out another way to restore the built in Explorer in
      this case

--- nvim/lua/plugins/neo_tree.lua
+++ nvim/lua/plugins/neo_tree.lua
@@ -9,7 +9,34 @@ return {
         },
       },
     },
+
+    -- This helps to show the new tree in full screen when open vim with a directory as argument
+    filesystem = {
+      hijack_netrw_behavior = "open_current",
+      -- hijack_netrw_behavior = "disabled",
+    },
   },
+
+  -- This disable neotree when neovim is started with directory as an argument.
+  -- init = function() end,
+
+  -- This shows the new tree in full screen when open vim with a directory as argument
+  init = function()
+    vim.api.nvim_create_autocmd("BufEnter", {
+      -- make a group to be able to delete it later
+      group = vim.api.nvim_create_augroup("NeoTreeInit", { clear = true }),
+      callback = function()
+        local f = vim.fn.expand("%:p")
+        if vim.fn.isdirectory(f) ~= 0 then
+          require("neo-tree")
+          -- vim.cmd("Neotree current dir=" .. f)
+          -- neo-tree is loaded now, delete the init autocmd
+          vim.api.nvim_clear_autocmds({ group = "NeoTreeInit" })
+        end
+      end,
+    })
+    -- keymaps
+  end,
 
   -- Disable default lazygit settings to hunt down the memory leak when moving and deleting files
   config = function(_, opts)
