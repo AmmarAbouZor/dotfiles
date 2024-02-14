172ec37676757e4b73297b4769e855bb52c5835d
Ammar Abou Zor <ammarabouzor@gmail.com>
2024-02-14 10:08:59 +0100

    Neovim: Add lines count to lua line
    - Remove unused location
    - Add pretty icon

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -5,5 +5,19 @@ return {
   opts = {
     theme = "catppuccin",
     options = { section_separators = "", component_separators = "" },
+
+    sections = {
+      lualine_y = {
+        { "progress", separator = " ", padding = { left = 1, right = 0 } },
+        {
+          function()
+            return " 󰉸 "
+          end,
+          padding = { left = 0, right = 0 },
+          separator = "",
+        },
+        { "%L", separator = " ", padding = { left = 0, right = 1 } },
+      },
+    },
   },
 }
