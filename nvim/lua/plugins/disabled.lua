77f26e1c1f2bd7b2fe10b64bdb4703f181ce3369
Ammar Abou Zor <ammarabouzor@gmail.com>
2024-07-21 12:12:54 +0200

    Nvim: Remove disabling spectre because it's not used anyway
    Lazyvim uses another plugin for that and I'm gonna leave it for now

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -4,9 +4,6 @@ return {
 
   -- { "folke/persistence.nvim", enabled = false },
 
-  -- search and replace is disabled until I need it
-  { "nvim-pack/nvim-spectre", enabled = false },
-
   -- I didn't like to navigate like this
   { "folke/flash.nvim", enabled = false },
 
