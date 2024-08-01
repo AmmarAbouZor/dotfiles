25db744adb057710a29216054bcc6a1fc2241f72
Ammar Abou Zor <ammar.abou.zor@accenture.com>
2024-08-01 11:54:58 +0200

    NeoVim: Trouble improvements
    - Auto Focus
    - Remove Tree view
    - Close when no diagnostics

--- nvim/lua/plugins/trouble.lua
+++ nvim/lua/plugins/trouble.lua
@@ -2,6 +2,17 @@ return {
   "folke/trouble.nvim",
   opts = {
     multiline = false, -- render multi-line messages
+    focus = true, -- Focus the window when opened
+    auto_close = true, -- auto close when there are no items
+
+    -- Remove the tree view list
+    modes = {
+      diagnostics = {
+        groups = {
+          { "filename", format = "{file_icon} {filename:Title} {count}" },
+        },
+      },
+    },
   },
   keys = {
     {
