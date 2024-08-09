b1eabbe3e0bae856295a410abd0e77ed477ea621
Ammar Abou Zor <ammar.abou.zor@accenture.com>
2024-08-09 17:49:13 +0200

    NoeVim: Disable auto pairs in comments

--- nvim/lua/plugins/MiniPairs.lua
+++ nvim/lua/plugins/MiniPairs.lua
@@ -1,6 +1,9 @@
 return {
   "echasnovski/mini.pairs",
   opts = {
+    -- disable auto pairs in strings and comments
+    skip_ts = { "string", "comment" },
+
     mappings = {
       -- restrict ' with < and & for rust
       ["'"] = { neigh_pattern = "[^%a\\|'|<|&]." },
