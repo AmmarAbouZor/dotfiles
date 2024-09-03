46c4179a4fa2c49701e9c3835e15648bc4d9c801
Ammar Abou Zor <ammarabouzor@gmail.com>
2024-09-03 19:33:20 +0200

    Neovim: Fix Telescope reference & Improve LSP bindings
    * Include current line in telescope references
    * Extend LSP keymap table using vim extend table method

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,84 +1,98 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  keys = {
-    {
-      "<leader>uc",
-      function()
-        -- Load all themes
-        require("tokyonight")
-        require("github-theme")
-        require("lazy").load({
-          plugins = { "gruvbox-material", "catppuccin", "everforest", "sonokai", "vscode.nvim" },
-        })
-        require("rose-pine")
-        require("gruvbox")
+  {
 
-        -- Then Open telescope themes picker
-        LazyVim.pick("colorscheme", { enable_preview = true })()
-      end,
-      desc = "Colorscheme with preview",
+    "nvim-telescope/telescope.nvim",
+    keys = {
+      {
+        "<leader>uc",
+        function()
+          -- Load all themes
+          require("tokyonight")
+          require("github-theme")
+          require("lazy").load({
+            plugins = { "gruvbox-material", "catppuccin", "everforest", "sonokai", "vscode.nvim" },
+          })
+          require("rose-pine")
+          require("gruvbox")
+
+          -- Then Open telescope themes picker
+          LazyVim.pick("colorscheme", { enable_preview = true })()
+        end,
+        desc = "Colorscheme with preview",
+      },
+
+      { "<leader>uC", false },
+      -- I'm using Diffview for that
+      -- {
+      --   "<leader>gf",
+      --   "<cmd>Telescope git_bcommits<cr>",
+      --   desc = "Current file history",
+      -- },
+
+      { "<leader>bb", "<cmd>Telescope buffers sort_mru=true<cr>", desc = "Buffers" },
+      { "<leader>,", "<cmd>Telescope buffers sort_mru=true<cr>", desc = "Buffers" },
+
+      {
+        "<leader>s/",
+        function()
+          require("telescope.builtin").grep_string({ search = vim.fn.input("Grep > "), root = false })
+        end,
+        mode = "n",
+        desc = "Grep word (cwd)",
+      },
+      {
+        "<leader>s.",
+        function()
+          require("telescope.builtin").grep_string({ search = vim.fn.input("Grep > ") })
+        end,
+        mode = "n",
+        desc = "Grep word (Root)",
+      },
+
+      {
+        "<leader>s?",
+        function()
+          local whole_word_under_cursor = vim.fn.expand("<cWORD>")
+          require("telescope.builtin").grep_string({ search = whole_word_under_cursor })
+        end,
+        mode = "n",
+        desc = "Search WORD",
+      },
+
+      { "<leader>sB", LazyVim.pick("live_grep", { grep_open_files = true }), desc = "Word (Open Buffers)" },
+
+      { "<leader>sD", LazyVim.pick("diagnostics", { sort_by = "severity" }), desc = "Workspace Diagnostics" },
+
+      { "<leader><space>", LazyVim.pick("files", { root = false }), desc = "Find Files (cwd)" },
     },
-
-    { "<leader>uC", false },
-    -- I'm using Diffview for that
-    -- {
-    --   "<leader>gf",
-    --   "<cmd>Telescope git_bcommits<cr>",
-    --   desc = "Current file history",
-    -- },
-
-    { "<leader>bb", "<cmd>Telescope buffers sort_mru=true<cr>", desc = "Buffers" },
-    { "<leader>,", "<cmd>Telescope buffers sort_mru=true<cr>", desc = "Buffers" },
-
-    {
-      "<leader>s/",
-      function()
-        require("telescope.builtin").grep_string({ search = vim.fn.input("Grep > "), root = false })
-      end,
-      mode = "n",
-      desc = "Grep word (cwd)",
+    opts = {
+      defaults = {
+        layout_strategy = "horizontal",
+        sorting_strategy = "ascending",
+        layout_config = {
+          horizontal = {
+            prompt_position = "top",
+          },
+        },
+        mappings = {
+          n = {
+            ["x"] = function(...)
+              return require("telescope.actions").delete_buffer(...)
+            end,
+          },
+        },
+      },
     },
-    {
-      "<leader>s.",
-      function()
-        require("telescope.builtin").grep_string({ search = vim.fn.input("Grep > ") })
-      end,
-      mode = "n",
-      desc = "Grep word (Root)",
-    },
-
-    {
-      "<leader>s?",
-      function()
-        local whole_word_under_cursor = vim.fn.expand("<cWORD>")
-        require("telescope.builtin").grep_string({ search = whole_word_under_cursor })
-      end,
-      mode = "n",
-      desc = "Search WORD",
-    },
-
-    { "<leader>sB", LazyVim.pick("live_grep", { grep_open_files = true }), desc = "Word (Open Buffers)" },
-
-    { "<leader>sD", LazyVim.pick("diagnostics", { sort_by = "severity" }), desc = "Workspace Diagnostics" },
-
-    { "<leader><space>", LazyVim.pick("files", { root = false }), desc = "Find Files (cwd)" },
   },
-  opts = {
-    defaults = {
-      layout_strategy = "horizontal",
-      sorting_strategy = "ascending",
-      layout_config = {
-        horizontal = {
-          prompt_position = "top",
-        },
-      },
-      mappings = {
-        n = {
-          ["x"] = function(...)
-            return require("telescope.actions").delete_buffer(...)
-          end,
-        },
-      },
-    },
+  {
+    "neovim/nvim-lspconfig",
+    opts = function()
+      local Keys = require("lazyvim.plugins.lsp.keymaps").get()
+      -- stylua: ignore
+      vim.list_extend(Keys, {
+        -- Include current line with references
+        { "gr", "<cmd>Telescope lsp_references include_current_line=true<cr>", desc = "References", nowait = true, noremap = true },
+      })
+    end,
   },
 }
