6363227eefc49f20da6ab67b7b34e9b2009fad29
Ammar Abou Zor <ammar.abou.zor@accenture.com>
2024-09-05 09:49:10 +0200

    Neovim: Replace harpoon functions from bufferline
    All needed functionality in harpoon can be used with bufferline by
    pinning the important buffer and have keybindings to the first few
    buffers so we reach them easily

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -1,11 +1,17 @@
 return {
   {
     "akinsho/bufferline.nvim",
-    opts = {
-      options = {
-        -- always_show_bufferline = true,
-        highlights = require("catppuccin.groups.integrations.bufferline").get(),
-      },
+
+    -- stylua: ignore
+    keys = {
+      { "<leader>k", mode = "n", desc = "GoTo Buffers" },
+      { "<leader>kk", function() require("bufferline").go_to(1, true) end, desc = "GoTo First Buffer" },
+      { "<leader>k1", function() require("bufferline").go_to(1, true) end, desc = "GoTo First Buffer" },
+      { "<leader>k2", function() require("bufferline").go_to(2, true) end, desc = "GoTo Second Buffer" },
+      { "<leader>k3", function() require("bufferline").go_to(3, true) end, desc = "GoTo Third Buffer" },
+      { "<leader>k4", function() require("bufferline").go_to(4, true) end, desc = "GoTo Fourth Buffer" },
+      { "<leader>k5", function() require("bufferline").go_to(5, true) end, desc = "GoTo Fifth Buffer" },
+      { "<leader>k0", function() require("bufferline").go_to(-1, true) end, desc = "GoTo Last Buffer" },
     },
   },
 }
