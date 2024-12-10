51f56f32232470bbcee63f37e475a90767d04841
Ammar Abou Zor <ammar.abou.zor@accenture.com>
2024-12-10 23:10:48 +0100

    nvim: Toggle snacks words & hack
    * Hack to start snacks words disabled by default.
    * Keybinding to toggle snacks words

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -1,9 +1,36 @@
 return {
   "folke/snacks.nvim",
+  init = function()
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "VeryLazy",
+      callback = function()
+        Snacks.words.enabled = true
+      end,
+    })
+
+    vim.api.nvim_create_autocmd("LspAttach", {
+      group = vim.api.nvim_create_augroup("Disable-Words-Start", { clear = true }),
+      once = true,
+      callback = function()
+        -- HACK: Keep tracking until it's fixed on main stream.
+        -- Dirty fix to disable words on start.
+        -- Currently, words calls enable() on LSP attach event which will return early if
+        -- enabled is already true. After two second we reset enabled to false, to make
+        -- toggling works.
+        vim.defer_fn(function()
+          Snacks.words.enabled = false
+        end, 2000)
+
+        Snacks.toggle.words():map("<leader>uv")
+      end,
+    })
+  end,
+
   opts = {
     words = {
-      enabled = false,
-      -- modes = { "n" }, -- modes to show references
+      -- Snacks can't be disabled by default if we want to toggle it later.
+      -- enabled = false,
+      modes = { "n" }, -- modes to show references
       -- debounce = 150, -- time in ms to wait before updating
     },
     notifier = { enabled = false },
