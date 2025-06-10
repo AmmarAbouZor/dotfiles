1a746995f87391839492bffa400edf5247bed7fe
Ammar Abou Zor <ammar.abou.zor@accenture.com>
2025-06-10 17:35:11 +0200

    Nvim: Move between buffers with leader

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -11,7 +11,17 @@ return {
       { "<leader>k3", function() require("bufferline").go_to(3, true) end, desc = "GoTo Third Buffer" },
       { "<leader>k4", function() require("bufferline").go_to(4, true) end, desc = "GoTo Fourth Buffer" },
       { "<leader>k5", function() require("bufferline").go_to(5, true) end, desc = "GoTo Fifth Buffer" },
-      { "<leader>k0", function() require("bufferline").go_to(-1, true) end, desc = "GoTo Last Buffer" },
+      { "<leader>k9", function() require("bufferline").go_to(-1, true) end, desc = "GoTo Last Buffer" },
+
+      { "<leader>1", function() require("bufferline").go_to(1, true) end, desc = "GoTo First Buffer" },
+      { "<leader>2", function() require("bufferline").go_to(2, true) end, desc = "GoTo Second Buffer" },
+      { "<leader>3", function() require("bufferline").go_to(3, true) end, desc = "GoTo Third Buffer" },
+      { "<leader>4", function() require("bufferline").go_to(4, true) end, desc = "GoTo Fourth Buffer" },
+      { "<leader>5", function() require("bufferline").go_to(5, true) end, desc = "GoTo Fifth Buffer" },
+      { "<leader>6", function() require("bufferline").go_to(6, true) end, desc = "GoTo Fifth Buffer" },
+      { "<leader>7", function() require("bufferline").go_to(7, true) end, desc = "GoTo Fifth Buffer" },
+      { "<leader>8", function() require("bufferline").go_to(8, true) end, desc = "GoTo Fifth Buffer" },
+      { "<leader>9", function() require("bufferline").go_to(-1, true) end, desc = "GoTo Last Buffer" },
     },
     opts = {
       options = {
